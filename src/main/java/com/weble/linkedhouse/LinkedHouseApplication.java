324289bb2ae1ad9def5b0830b0fa1a157a969eaf
astrataraxia <astrataraxia@gmail.com>
2023-07-28 15:21:48 +0900

    #9 패키지 통합

---
+++ src/main/java/com/weble/linkedhouse/LinkedHouseApplication.java
@@ -0,0 +1,13 @@
+package com.weble.linkedhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weble/linkedhouse/LinkedHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weble.linkedhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
