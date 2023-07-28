c7118336e82e10fd8c128fbbc21d2c67d61f49e9
stone <109290247+astrataraxia@users.noreply.github.com>
2023-07-28 15:35:02 +0900

    root package

---
+++ src/main/java/com/weble/linkedhous/LinkedHousApplication.java
@@ -0,0 +1,13 @@
+package com.weble.linkedhous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedHousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedHousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weble/linkedhous/LinkedHousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weble.linkedhous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedHousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
