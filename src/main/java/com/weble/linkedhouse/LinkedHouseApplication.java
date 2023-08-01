9a247a5a456fe1849af4daaa59609ff098ec0c5a
melamond <mikisa0813@gmail.com>
2023-08-01 11:43:14 +0900

    melamond : 숙박업소 정보 도메인 구현 완료

--- src/main/java/com/weble/linkedhouse/LinkedHouseApplication.java
+++ src/main/java/com/weble/linkedhouse/LinkedHouseApplication.java
@@ -1,13 +1,13 @@
-package com.weble.linkedhous;
+package com.weble.linkedhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkedHousApplication {
+public class LinkedHouseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LinkedHousApplication.class, args);
+		SpringApplication.run(LinkedHouseApplication.class, args);
 	}
 
 }
