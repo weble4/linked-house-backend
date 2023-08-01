989eb09dd812314fcf2af1883491030f41150e31
astrataraxia <astrataraxia@gmail.com>
2023-08-01 10:36:15 +0900

    customer profile 및 관련 클래스 수정
    AuditingFields에서 잘못된 부분 수정 및 customer profile관련 클래스들 세세한 부분  수정

--- src/main/java/com/weble/linkedhouse/customer/entity/constant/PublicAt.java
+++ src/main/java/com/weble/linkedhouse/customer/entity/constant/PublicAt.java
@@ -1,5 +1,15 @@
 package com.weble.linkedhouse.customer.entity.constant;
 
+import lombok.Getter;
+
 public enum PublicAt {
-    PUBLIC, PRIVATE;
+    PUBLIC("public"),
+    PRIVATE("private");
+
+    @Getter
+    private final String reasonPhrase;
+
+    PublicAt(String reasonPhrase) {
+        this.reasonPhrase = reasonPhrase;
+    }
 }
--- src/main/java/com/weble/linkedhouse/util/AuditingFields.java
+++ src/main/java/com/weble/linkedhouse/util/AuditingFields.java
@@ -7,13 +7,14 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Getter
 @ToString
-@EntityListeners(AuditingFields.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class AuditingFields {
 
