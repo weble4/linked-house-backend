8c614833acbbc90254259d2e7836265621cd6b21
astrataraxia <astrataraxia@gmail.com>
2023-08-02 20:54:52 +0900

    예약 취소 엔티티 테이블 작성
    예약 취소 테이블 작성

---
+++ src/main/java/com/weble/linkedhouse/reservation/entity/CancelReservation.java
@@ -0,0 +1,39 @@
+package com.weble.linkedhouse.reservation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CancelReservation {
+
+    @Id
+    @Column(name = "cancel_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cancelId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservationId")
+    private Reservation reservation;
+
+    @Column(name = "checkin_date", nullable = false)
+    private LocalDateTime checkinDate;
+
+    @Column(name = "checkout_date", nullable = false)
+    private LocalDateTime checkoutDate;
+
+    @Column(name = "reservation_num", nullable = false)
+    private int reservationNum;
+}
