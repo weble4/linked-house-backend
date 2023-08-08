6c3477dc22aa306c3c070811917ac2c68a63c90f
Letyon <kover25@naver.com>
2023-08-08 15:11:49 +0900

    review repository
    리포지토리 작성 완료

---
+++ src/main/java/com/weble/linkedhouse/review/repository/FeedbackCustomerRepository.java
@@ -0,0 +1,12 @@
+package com.weble.linkedhouse.review.repository;
+
+import com.weble.linkedhouse.review.entity.FeedbackCustomer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FeedbackCustomerRepository extends JpaRepository<FeedbackCustomer, Long> {
+
+    Optional<FeedbackCustomer> findByCustomerId(String CustomerId);
+
+}
