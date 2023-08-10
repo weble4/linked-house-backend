15d426dd83394824a9181e53689072ebae000705
Jimmydevi <kimuudev@gmail.com>
2023-08-10 14:45:18 +0900

    Revert "notification API 작성완료."
    This reverts commit 8f18402978ee31498066eb5fba5dc80cb4ae1e8e.

--- src/main/java/com/weble/linkedhouse/notification/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.weble.linkedhouse.notification.repository;
-
-import com.weble.linkedhouse.notification.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
