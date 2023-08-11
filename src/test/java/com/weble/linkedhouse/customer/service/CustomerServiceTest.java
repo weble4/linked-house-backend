a1ea29cddc96456f2797c2501b2bc895e7405826
astrataraxia <astrataraxia@gmail.com>
2023-08-11 17:15:21 +0900

    #81-workfolw-action: Auto stash before revert of "Github action build와 관련되어 redis 서버 문제"

--- src/test/java/com/weble/linkedhouse/customer/service/CustomerServiceTest.java
+++ src/test/java/com/weble/linkedhouse/customer/service/CustomerServiceTest.java
@@ -25,7 +25,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
@@ -61,9 +60,6 @@ class CustomerServiceTest {
     // JavaMailSender 빈을 모킹합니다.
     @MockBean
     JavaMailSender javaMailSender;
-
-    @MockBean
-    private RedisTemplate<String, Object> redisTemplate;
 
     @BeforeEach
     void setUp() {
