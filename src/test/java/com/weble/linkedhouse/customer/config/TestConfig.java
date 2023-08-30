8bb6bfad27d4810e787a3bd5f25e0f30dfd4a002
astrataraxia <astrataraxia@gmail.com>
2023-08-30 17:26:23 +0900

    test 관련 문제로 인해 수정

--- src/test/java/com/weble/linkedhouse/customer/config/TestConfig.java
+++ src/test/java/com/weble/linkedhouse/customer/config/TestConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 
-@Configuration
+//@Configuration
 public class TestConfig {
-    @Bean
-    public JavaMailSender javaMailSender() {
-        return Mockito.mock(JavaMailSender.class);
-    }
+//    @Bean
+//    public JavaMailSender javaMailSender() {
+//        return Mockito.mock(JavaMailSender.class);
+//    }
 }
