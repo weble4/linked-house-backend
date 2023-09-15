5e3f71533f1c125ce42700f49a03a14c411b6be9
melamond <mikisa0813@gmail.com>
2023-09-15 17:08:44 +0900

    fix : /api/houses permit

--- src/main/java/com/weble/linkedhouse/security/SecurityConfig.java
+++ src/main/java/com/weble/linkedhouse/security/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
             "/api/customers/activate-state",
             "/api/customers/check-email",
             "/api/customers/reissue",
+            "/api/houses",
     };
 
     @Bean
